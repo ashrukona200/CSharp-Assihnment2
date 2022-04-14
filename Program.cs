dac0d62e387eb4292560ba6d5e33e16851585bfd
Ashrukona Maiti <59428612+ashrukona200@users.noreply.github.com>
2022-04-14 16:04:33 +0530

    Commit 2

--- Program.cs
+++ Program.cs
@@ -1,158 +1,176 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-namespace Assignment
+
+namespace ASSIGNMENT2
 {
-    public class Program
+    public class ProductsList
     {
-        string ProductName;
-        double ProductPrice;
-        int ProductQuantity;
-        string ProductType;
+        string Name;
+        double Price;
+        int Quantity;
+        string Type;
 
         public string proName
         {
-            get { return ProductName; }
-            set { ProductName = value; }
+            get { return Name; }
+            set { Name = value; }
         }
         public double proPrice
         {
-            get { return ProductPrice; }
-            set { ProductPrice = value; }
+            get { return Price; }
+            set { Price = value; }
         }
         public int proQuantity
         {
-            get { return ProductQuantity; }
-            set { ProductQuantity = value; }
+            get { return Quantity; }
+            set { Quantity = value; }
         }
 
         public string proType
         {
-            get { return ProductType; }
-            set { ProductType = value; }
+            get { return Type; }
+            set { Type = value; }
         }
 
         public override string ToString()
         {
-            return ($"{proName}, {proPrice}, {proQuantity}, {proType}");
+            return ($"{proName}, {proPrice} RS, {proQuantity}, {proType}");
         }
 
 
         static void Main(string[] args)
         {
-            List<Program> newProductList = new List<Program>()
-            {
-                new Program(){
-                    proName = "lettuce", 
-                    proPrice = 10.5, 
-                    proQuantity = 50, 
+            List<ProductsList> newProductList = new List<ProductsList>(){
+                new ProductsList(){
+                    proName = "lettuce",
+                    proPrice = 10.5,
+                    proQuantity = 50,
                     proType = "Leafy green"
                 },
-                new Program(){
-                    proName = "cabbage", 
-                    proPrice = 20, 
-                    proQuantity = 100, 
+                new ProductsList(){
+                    proName = "cabbage",
+                    proPrice = 20,
+                    proQuantity = 100,
                     proType = "Cruciferous"
                 },
-                new Program(){ 
-                    proName = "pumpkin", 
-                    proPrice = 30, 
-                    proQuantity = 30, 
+                new ProductsList(){
+                    proName = "pumpkin",
+                    proPrice = 30,
+                    proQuantity = 30,
                     proType = "Marrow"
                 },
-                new Program(){ 
-                    proName = "cauliflower", 
-                    proPrice = 10, 
-                    proQuantity = 25, 
+                new ProductsList(){
+                    proName = "cauliflower",
+                    proPrice = 10,
+                    proQuantity = 25,
                     proType = "Cruciferous"
                 },
-                new Program(){ 
-                    proName = "zucchini", 
-                    proPrice = 20.5, 
-                    proQuantity = 50, 
+                new ProductsList(){
+                    proName = "zucchini",
+                    proPrice = 20.5,
+                    proQuantity = 50,
                     proType = "Marrow"
                 },
-                new Program(){ 
-                    proName = "yam", 
-                    proPrice = 30, 
-                    proQuantity = 50, 
+                new ProductsList(){
+                    proName = "yam",
+                    proPrice = 30,
+                    proQuantity = 50,
                     proType = "Root"
                 },
-                new Program(){ 
-                    proName = "spinach", 
-                    proPrice = 10, 
-                    proQuantity = 100, 
+                new ProductsList(){
+                    proName = "spinach",
+                    proPrice = 10,
+                    proQuantity = 100,
                     proType = "Leafy green"
                 },
-                new Program(){ 
-                    proName = "broccoli", 
-                    proPrice = 20.2 , 
-                    proQuantity = 75, 
+                new ProductsList(){
+                    proName = "broccoli",
+                    proPrice = 20.2,
+                    proQuantity = 75,
                     proType = "Cruciferous"
                 },
-                new Program(){ 
-                    proName = "garlic", 
-                    proPrice = 30 , 
-                    proQuantity = 20, 
+                new ProductsList(){
+                    proName = "garlic",
+                    proPrice = 30,
+                    proQuantity = 20,
                     proType = "Leafy green"
                 },
-                new Program(){
-                    proName = "silverbeet", 
-                    proPrice = 10, 
-                    proQuantity = 50, 
+                new ProductsList(){
+                    proName = "silverbeet",
+                    proPrice = 10,
+                    proQuantity = 50,
                     proType = "Marrow"
                 }
             };
 
-            Console.WriteLine("Total number of products in the list : {0}\n", newProductList.Count);
+            //Printing the total number of products in the list---------------
+            int total = newProductList.Count;
+            Console.WriteLine($"Total number of products in the list : {total}\n");
 
-            newProductList.Add(new Program(){ 
-                proName = "Potato", 
-                proPrice = 10, 
-                proQuantity = 50, 
-                proType = "Root" 
+
+            //Adding a new product (Potato,10RS, 50, Root)-----------------
+            newProductList.Add(new ProductsList()
+            {
+                proName = "Potato",
+                proPrice = 10,
+                proQuantity = 50,
+                proType = "Root"
             });
-
+            //printing the list of all products---------------------
             Console.WriteLine("After adding new product (Potato,10RS, 50, Root), Now new Products are : ");
             foreach (var product in newProductList)
+            {
                 Console.WriteLine(product);
-            Console.WriteLine("\nTotal number of Product in new Product List: {0}\n", newProductList.Count);
+            }
+            //Printing total number of products---------------
+            total = newProductList.Count;
+            Console.WriteLine($"\nTotal number of Product in new Product List: {total}\n");
 
+
+            //Printing all the products of which have the type Leafy green-----------------
             Console.WriteLine("Products of which have the type Leafy green are : ");
             foreach (var product in newProductList)
             {
                 if (product.proType.Equals("Leafy green"))
+                {
                     Console.WriteLine(product);
-            }
-
-            newProductList.RemoveAt(9);
-            Console.WriteLine("\nAs all the garlic is sold out, after removing Garlic from the list, now total number of products that are left on the list : {0}", newProductList.Count);
-
-            foreach (var product in newProductList)
-            {
-                if(product.proName.Equals("cabbage")){
-                    product.proQuantity = product.proQuantity +50;
-                    Console.WriteLine("\nIf the user adds 50 cabbages to the inventory, then the final quantity of cabbage in the inventory : " + product);
                 }
             }
 
-            double totalPrice = 0;
+            //As all the garlic is sold out, Removing Garlic from the list and print the total number of products that are left on the list------------
+            newProductList.RemoveAt(9);
+            total = newProductList.Count;
+            Console.WriteLine($"\nAs all the garlic is sold out, after removing Garlic from the list, now total number of products that are left on the list : {total}");
+
+
+            //If the user adds 50 cabbages to the inventory, printing the final quantity of cabbage in the inventory------------------
+            foreach (var product in newProductList)
+            {
+                if (product.proName.Equals("cabbage"))
+                {
+                    product.proQuantity = product.proQuantity + 50;
+                    Console.WriteLine($"\nIf the user adds 50 cabbages to the inventory, then the final quantity of cabbage in the inventory : {product}");
+                }
+            }
+
+            double price = 0;
             foreach (var product in newProductList)
             {
                 if (product.proName.Equals("lettuce"))
                 {
-                    totalPrice = totalPrice + (1 * product.proPrice);
+                    price = price + (1 * product.proPrice);
                 }
                 if (product.proName.Equals("zucchini"))
                 {
-                    totalPrice = totalPrice + (2 * product.proPrice);
+                    price = price + (2 * product.proPrice);
                 }
                 if (product.proName.Equals("broccoli"))
                 {
-                    totalPrice = totalPrice + (1 * product.proPrice);
+                    price = price + (1 * product.proPrice);
                 }
             }
-            Console.WriteLine("\nIf a user purchases 1kg lettuce, 2 kg zucchini, 1 kg broccoli then the user needs to pay : {0} RS", Math.Round(totalPrice));
+            Console.WriteLine($"\nIf a user purchases 1kg lettuce, 2 kg zucchini, 1 kg broccoli then the user needs to pay : {Math.Round(price)} RS");
         }
     }
 }
